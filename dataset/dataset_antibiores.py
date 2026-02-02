69fdac80b7ced38bf2dcb424baa234f126ba7e5c
leoschn <schneider.leo@free.fr>
2026-02-02 15:53:10 +0100

    fix model

--- dataset/dataset_antibiores.py
+++ dataset/dataset_antibiores.py
@@ -82,7 +82,7 @@ class To_float:
 
 def pkl_loader(path):
     with open(path, 'rb') as f:
-        sample = torch.from_numpy(pkl.load(f))
+        sample = torch.from_numpy(pkl.load(f)['image'])
     sample = sample.unsqueeze(0)
     return sample
 
