2a1a9da72cb928a39586bbda35987e0aefc8b413
leoschn <schneider.leo@free.fr>
2026-04-20 13:08:31 +0200

    fix main

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from config import load_args, load_args_species
-from train import run
+from train import run_species
 
 if __name__ == '__main__':
     args = load_args_species()
-    run(args)
+    run_species(args)
