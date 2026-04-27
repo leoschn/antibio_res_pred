2cb3995eaa6f14c6efd002b5d92ef3134c514741
leoschn <schneider.leo@free.fr>
2026-04-27 12:53:37 +0200

    add split_dataset

--- dataset/dataset_antibiores.py
+++ dataset/dataset_antibiores.py
@@ -2,102 +2,53 @@ import glob
 import os
 import pickle as pkl
 import re
-from skimage import measure
 import numpy as np
-import pandas as pd
 import torchvision.transforms as transforms
 from torchvision.datasets import DatasetFolder
 import torch
 
-class Random_erasing2:
-    """with a probability prob, erases a proportion prop of the image"""
-
-    def __init__(self, prob, prop):
-        self.prob = prob
-        self.prop = prop
-
-    def __call__(self, x):
-        if np.random.rand() < self.prob:
-            labels = measure.label(x.numpy() > 0, connectivity=1)
-            regions = measure.regionprops(labels)
-            pics_suppr = np.random.rand(len(regions)) < self.prop
-            for k in range(len(regions)):
-                if pics_suppr[k]:
-                    try:
-                        _, y1, x1, _, y2, x2 = regions[k].bbox
-                    except:
-                        raise Exception(regions[k].bbox)
-                    x[:, y1:y2, x1:x2] *= regions[k].image == False
-            return x
-
-        return x
-
-
-class Random_int_noise:
-    """With a probability prob, adds a gaussian noise to the image """
-
-    def __init__(self, prob, maximum):
-        self.prob = prob
-        self.minimum = 1 / maximum
-        self.delta = maximum - self.minimum
-
-    def __call__(self, x):
-        if np.random.rand() < self.prob:
-            return x * (self.minimum + torch.rand_like(x) * self.delta)
-        return x
-
-
-class Random_shift_rt:
-    """With a probability prob, shifts verticaly the image depending on a gaussian distribution"""
-
-    def __init__(self, prob, mean, std):
-        self.prob = prob
-        self.mean = torch.tensor(float(mean))
-        self.std = float(std)
-
-    def __call__(self, x):
-        if np.random.rand() < self.prob:
-            shift = torch.normal(self.mean, self.std)
-            return transforms.functional.affine(x, 0, [0, shift], 1, [0, 0])
-        return x
-
-class Log_normalisation:
-    """Log normalisation of intensities"""
-
-    def __init__(self, eps=1e-5):
-        self.epsilon = eps
-
-    def __call__(self, x):
-        return torch.log(x+1+self.epsilon)/torch.log(torch.max(x)+1+self.epsilon)
-
-
-class To_float:
-    "Repeat tensor along new dim"
-
-    def __init__(self):
-        pass
-
-    def __call__(self, x):
-        return x.to(torch.float32)
 
 def pkl_loader(path):
     with open(path, 'rb') as f:
         sample = pkl.load(f)
     return sample
 
+class CropTransform:
+    def __init__(self, top: int, left: int, height: int, width: int):
+        self.top = top
+        self.left = left
+        self.height = height
+        self.width = width
+
+    def __call__(self, image: np.ndarray) -> np.ndarray:
+        if image.ndim == 2:  # (H, W)
+            return image[self.top:self.top + self.height,self.left:self.left + self.width]
+        elif image.ndim == 3:  # (C, H, W)
+            return image[:,self.top:self.top + self.height,self.left:self.left + self.width]
+        else:
+            raise ValueError("Unsupported image shape")
+
 
 class SpeciesDataset(DatasetFolder):
-    def __init__(self, root, included_species):
 
+    def __init__(self, root, origin, im_size = (256,512)):
         self.root = root
-        self.samples, self.targets , self.sample_name= self.build_dataset('.pkl',included_species)
+        self.samples, self.targets , self.sample_name= self.build_dataset('.pkl')
         self.loader = pkl_loader
-        self.classes = list(set(included_species))
+        self.classes = list(set(self.targets))
         self.classes.sort()
-        print(self.classes)
-
-        self.transform = transforms.Compose([transforms.Resize((256, 256)),])
-        self.transform_tensor = transforms.Compose([transforms.ToTensor(),transforms.Resize((256, 256))])
+        self.origin = origin
+        if origin == 'real' :
+            self.transform_img = transforms.Compose([
+                                                 CropTransform(top=90, left=0, height=422, width=1024),
+                                                 transforms.ToTensor(),
+                                                 transforms.Resize(im_size),
+                                                 transforms.Normalize((3.04),(3.04)), #same as the transform used in diffusion
+            ])
+        elif origin == 'synth' :
+            self.transform_img = None
+        else :
+            raise NotImplementedError
 
 
     def __getitem__(self, index: int):
@@ -106,14 +57,14 @@ class SpeciesDataset(DatasetFolder):
         name = self.sample_name[index]
         sample = self.loader(path)
         sample = sample["image"]
-        if len(sample)==101 : #MS1 + 100 MS2
+        if self.origin == 'real':
+            assert len(sample)==101
             sample = sample[1:101] #only keep ms2 data
+        else :
+            assert len(sample==100)
 
-        if self.transform:
-            if type(sample[0])==torch.Tensor:
-                sample = [self.transform(s) for s in sample]
-            else :
-                sample = [self.transform_tensor(s) for s in sample]
+        if self.transform_img:
+                sample = [self.transform_img(s) for s in sample]
         sample = torch.cat(sample, dim=0)
         label = self.classes.index(label)
         return sample, label, name
@@ -123,13 +74,13 @@ class SpeciesDataset(DatasetFolder):
         return len(self.samples)
 
 
-    def build_dataset(self, valid_ext, included_species):
+    def build_dataset(self, valid_ext):
         instances,labels,sample_name=[],[],[]
         file_names = glob.glob(os.path.join(self.root, '*'))
         for file_name in file_names:
             if file_name.endswith(valid_ext):
                 m = re.match(r'([A-Z]+)-(\d+)-([A-Z]+)',  os.path.basename(file_name))
-                if m and m.group(1) in included_species :
+                if m and m.group(1) :
                     instances.append(file_name)
                     labels.append(m.group(1))
                     sample_name.append(f"{m.group(1)}-{m.group(2)}-{m.group(3)}")
@@ -137,82 +88,3 @@ class SpeciesDataset(DatasetFolder):
         print(len(instances), ' image detected. \n Dataset loading: done')
         return instances,labels,sample_name
 
-
-class Antibio_Dataset(DatasetFolder):
-    def __init__(self, root,label_path,label_col,augment=False, model_type=None):
-        self.root = root
-        self.instances, self.labels , self.sample_name= self.make_dataset('.pkl',label_path,label_col)
-
-        self.loader = pkl_loader
-        self.type = model_type
-        if self.type == 'ms1':
-            if augment:
-                self.transform_img = transforms.Compose([transforms.ToTensor(),
-                                                         transforms.Resize((256,256)),
-                                                         transforms.Normalize((0.246), (0.210)),
-                                                         Random_shift_rt(1,0,3),
-                                                         Random_int_noise(1, 2)])
-
-            else :
-                self.transform_img = transforms.Compose([transforms.ToTensor(),
-                                                         transforms.Resize((256,256)),
-                                                         transforms.Normalize((0.246), (0.210)),])
-        elif self.type == 'ms2':
-            if augment:#augment 3D a définir
-                self.transform_img = transforms.Compose([transforms.ToTensor(),
-                                                         transforms.Resize((256, 256)),
-                                                         transforms.Normalize((1.0207), (1.0011)),
-                                                         Random_shift_rt(1, 0, 3),
-                                                         Random_int_noise(1, 2)])
-
-            else:
-                self.transform_img = transforms.Compose([transforms.ToTensor(),
-                                                         transforms.Resize((256, 256)),
-                                                         transforms.Normalize((1.0207), (1.0011)),
-                                                        ])
-        else :
-            self.transform_img = None
-        self.label_col = label_col
-        self.classes = list(set(self.labels))
-        self.classes.sort()
-
-
-    def __getitem__(self, index: int):
-        label = self.labels[index]
-        path = self.instances[index]
-        name = self.sample_name[index]
-        sample = self.loader(path)
-        if self.type == 'ms2':
-            sample = sample["image"]
-            sample = [s[:,0:899] for s in sample] #some image are 633*901/900 instead of 633*899
-            sample = np.stack(sample, axis=0)
-        if self.transform_img is not None:
-            sample = self.transform_img(sample)
-        label_id = self.classes.index(label)
-        return sample, label_id, name
-
-
-    def __len__(self):
-        return len(self.instances)
-
-
-    def make_dataset(self,valid_ext,label_path,label_col):
-        instances,labels,sample_name=[],[],[]
-        df_label = pd.read_csv(label_path)
-        file_names = glob.glob(os.path.join(self.root, '*'))
-        for file_name in file_names:
-            if file_name.endswith(valid_ext):
-                m = re.match(r'([A-Z]+)-(\d+)-([A-Z]+)',  os.path.basename(file_name))
-                if m:
-                    label = df_label[df_label['sample_name'] == f"{m.group(1)}{m.group(2)}"][label_col].tolist()
-                    if label_col == 'species':
-                        label = [i.split(' ')[0]+ ' ' + i.split(' ')[0] for i in label]
-                    if len(label) > 0 :
-                        instances.append(file_name)
-                        labels.append(label[0])
-                        sample_name.append(f"{m.group(1)}-{m.group(2)}-{m.group(3)}")
-                else:
-                    raise ValueError(f"Label not found for: {file_name}")
-        assert(len(instances)==len(labels))
-        print(len(instances), ' image detected. \n Dataset loading: done')
-        return instances,labels,sample_name
---
+++ split_dataset.py
@@ -0,0 +1,99 @@
+import glob
+import os
+import numpy as np
+import shutil
+
+DIR_IMG = '/lustre/fsn1/projects/rech/bun/ucg81ws/image'
+DIR_PAIRS = '/lustre/fsn1/projects/rech/bun/ucg81ws/pairs'
+
+OUT_DIR_PAIRS = '/lustre/fsn1/projects/rech/bun/ucg81ws/synt_class_dataset/eval_1/generation_dataset'
+OUT_DIR_IMG = '/lustre/fsn1/projects/rech/bun/ucg81ws/synt_class_dataset/eval_1/classification_dataset'
+
+MAJOR_SPECIES = ['CITFRE','ESCCOL','KLEPNE','ENTHOR','PRTMIR']
+
+img_f_name_list = glob.glob(os.path.join(DIR_IMG,'*.pkl'))
+
+img_base_name_list = [f_name.base_name.split('.')[0] for f_name in img_f_name_list]
+img_base_name_array = np.array(img_base_name_list)
+
+
+def is_major_species(name,species):
+    for spe in species:
+        if spe in name:
+            return True
+        else:
+            return False
+
+img_is_major = np.array([is_major_species(f_name,MAJOR_SPECIES) for f_name in img_base_name_list])
+
+path_major_img_array = img_base_name_array[img_is_major]
+
+ind_major_array = np.linspace(0,len(path_major_img_array),len(path_major_img_array))
+
+np.random.shuffle(ind_major_array)
+
+idx_A = int(len(path_major_img_array)*0.4)
+idx_B = idx_A + int(len(path_major_img_array)*0.1)
+idx_C = idx_B + int(len(path_major_img_array)*0.2)
+idx_D = idx_C + int(len(path_major_img_array)*0.05)
+idx_E = len(path_major_img_array)
+
+
+path_minor_img_array = img_base_name_array[~img_is_major]
+
+ind_minor_array = np.linspace(0,len(path_minor_img_array),len(path_minor_img_array))
+
+np.random.shuffle(ind_minor_array)
+
+idx_C_minor = int(len(path_minor_img_array)*0.8)
+idx_D_minor = len(path_minor_img_array)
+
+path_array_A = path_major_img_array[idx_A]
+path_array_B = path_major_img_array[idx_B]
+path_array_C = np.concat([path_major_img_array[idx_C],path_minor_img_array[idx_C_minor]], axis=0)
+path_array_D = np.concat([path_major_img_array[idx_D],path_minor_img_array[idx_D_minor]], axis=0)
+path_array_E = path_major_img_array[idx_E]
+
+print('Split A : ', len(path_array_A), 'images')
+print('Split B : ', len(path_array_B), 'images')
+print('Split C : ', len(path_array_C), 'images')
+print('Split D : ', len(path_array_D), 'images')
+print('Split E : ', len(path_array_E), 'images')
+
+#COPY IMG
+for base_name in path_array_A:
+    shutil.copy(os.path.join(DIR_IMG,base_name),os.path.join(OUT_DIR_IMG,'split_A',base_name,'.pkl'))
+for base_name in path_array_B:
+    shutil.copy(os.path.join(DIR_IMG,base_name),os.path.join(OUT_DIR_IMG,'split_B',base_name,'.pkl'))
+
+
+for base_name in path_array_E:
+    shutil.copy(os.path.join(DIR_IMG,base_name),os.path.join(OUT_DIR_IMG,'split_E',base_name,'.pkl'))
+
+#COPY PAIRS
+
+for base_name in path_array_C:
+    for i in range(100):
+        try:
+            shutil.copy(os.path.join(DIR_PAIRS, base_name),
+                        os.path.join(OUT_DIR_PAIRS, 'split_C', base_name, f'_ms2_{i}.pkl'))
+        except:
+            pass
+for base_name in path_array_D:
+    for i in range(100):
+        try:
+            shutil.copy(os.path.join(DIR_PAIRS, base_name),
+                        os.path.join(OUT_DIR_PAIRS, 'split_D', base_name, f'_ms2_{i}.pkl'))
+        except:
+            pass
+for base_name in path_array_E:
+    for i in range(100):
+        try:
+            shutil.copy(os.path.join(DIR_PAIRS, base_name),
+                        os.path.join(OUT_DIR_PAIRS, 'split_E', base_name, f'_ms2_{i}.pkl'))
+        except:
+            pass
+
+
+
+
