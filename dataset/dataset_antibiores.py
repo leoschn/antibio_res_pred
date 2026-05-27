f278701b7c115f62375f673068b4abe193ca09e0
leoschn <schneider.leo@free.fr>
2026-05-27 12:20:06 +0200

    fix reconstruction

--- dataset/dataset_antibiores.py
+++ dataset/dataset_antibiores.py
@@ -27,9 +27,8 @@ def reconstruct_synth(input_dir, output_dir,valid_ext,epoch):  # one file per wi
                 wind = pkl_loader(base_name+f'ms2_{i}_{epoch}.pkl')
                 list_img.append(wind)
             sample = {'image': list_img}
-            print('saving ',os.path.join(output_dir, os.path.basename(base_name)+'_reconstructed.pkl'))
-            # pkl.dump(sample, open(os.path.join(output_dir, os.path.basename(base_name)+'_reconstructed.pkl'), 'wb'))
-            break
+            print('saving ',os.path.join(output_dir, os.path.basename(base_name)+'reconstructed.pkl'))
+            pkl.dump(sample, open(os.path.join(output_dir, os.path.basename(base_name)+'reconstructed.pkl'), 'wb'))
 
 class CropTransform:
     def __init__(self, top: int, left: int, height: int, width: int):
