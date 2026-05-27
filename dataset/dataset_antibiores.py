e7976c5b74b55c4425387b8c3ad67880f86c1863
leoschn <schneider.leo@free.fr>
2026-05-27 11:57:54 +0200

    fix reconstruction

--- dataset/dataset_antibiores.py
+++ dataset/dataset_antibiores.py
@@ -20,11 +20,11 @@ def reconstruct_synth(input_dir, output_dir,valid_ext,epoch):  # one file per wi
     os.makedirs(output_dir, exist_ok=True)
     for sample_name in sample_names:
         if sample_name.endswith(valid_ext):
-            base_name = os.path.join(os.path.dirname(sample_name),'_'.join(os.path.basename(sample_name).removesuffix('.pkl').split('_')[:-2]))
+            base_name = os.path.join(os.path.dirname(sample_name),'_'.join(os.path.basename(sample_name).removesuffix('.pkl').split('ms2')[0]))
             list_img=[]
-            print('Processing ', base_name + f'_X_{epoch}.pkl')
+            print('Processing ', base_name + f'ms2_X_{epoch}.pkl')
             for i in range(100):
-                wind = pkl_loader(base_name+f'_{i}_{epoch}.pkl')
+                wind = pkl_loader(base_name+f'ms2_{i}_{epoch}.pkl')
                 list_img.append(wind)
             sample = {'image': list_img}
             pkl.dump(sample, open(os.path.join(output_dir, base_name+'_reconstructed.pkl'), 'wb'))
